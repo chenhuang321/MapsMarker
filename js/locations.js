178cc0659bd4611e473048ddee67c720caee4962
Chen Huang <jameshuang7788@gmail.com>
2018-08-08 17:40:32 -0400

    Initial commit

---
+++ js/locations.js
@@ -0,0 +1,109 @@
+const locations = [
+  {
+    street: 'Finch Station, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Don Mills Station, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Sheppard-Yonge Station, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Sheppard West Station, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Seneca College, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Seneca@York, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'York University, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'University of Toronto, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Toronto Zoo, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Union Station, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'CN Tower, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Central Island, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Perason Airport, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Fairview Mall, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Eaton Centre, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Pacific Mall, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Yorkdale, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'IKEA North York, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Chinatown, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  },
+  {
+    street: 'Centennial College, Toronto',
+    state: 'ON',
+    country: 'Canada'
+  }
+];
+
+/**
+ * @return { mixed } Location data for Google Map Marker
+ */
+function getLocations() {
+  return locations;
+}
